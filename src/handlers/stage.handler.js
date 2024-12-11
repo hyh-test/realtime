6c42fb16f6774feec9c107cdfefe043e249cf9ad
황종하 <whdgk3615@naver.com>
2024-12-11 17:49:44 +0900

    [fix]

--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -12,20 +12,19 @@ export const moveStageHandler = (userId, payload) => {
   currentStages.sort((a, b) => a.id - b.id);
   const currentStage = currentStages[currentStages.length - 1];
 
-  // 클라이언트 와 서버  비교
+  // payload 의 currentStage 와 비교
   if (currentStage.id !== payload.currentStage) {
     return { status: 'fail', message: 'Current stage mismatch' };
   }
 
   // 점수 검증
-  const serverTime = Date.now();//현재 타임스탬프
+  const serverTime = Date.now();
   const elapsedTime = (serverTime - currentStage.timestamp) / 1000; // 초 단위로 계산
 
-  
   // 1초당 1점, 100점이상 다음스테이지 이동, 오차범위 5
   // 클라이언트와 서버 간의 통신 지연시간을 고려해서 오차범위 설정
   // elapsedTime 은 100 이상 105 이하 일 경우만 통과
-  if (elapsedTime < 10 || elapsedTime > 10.5) {
+  if (elapsedTime < 100 || elapsedTime > 105) {
     return { status: 'fail', message: 'Invalid elapsed time' };
   }
 
